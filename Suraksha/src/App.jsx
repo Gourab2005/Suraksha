74eadeb339db5d7d841cb7298ba3c53474fbefc2
subhajit11010 <142614931+subhajit11010@users.noreply.github.com>
2024-08-24 16:39:49 +0530

    "7"

---
+++ Suraksha/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from './Pages/Home'
+import Loggedin from './Pages/Loggedin'
+import Admindashboard from './Pages/Admindashboard'
+import Login from './Pages/Login'
+import Signup from './Pages/signup'
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Home/>,
+    },
+    {
+      path: "/login",
+      element:<Login/>,
+    },
+    {
+        path: "/signup",
+        element: <Signup />
+    },
+    {
+      path: "/loggedin",
+      element:<Loggedin/>,
+    },
+    {
+      path: "/admindashboard",
+      element:<Admindashboard/>,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+export default App
---
+++ Suraksha/src/Pages/Login.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { account } from "../appwrite/appwriteConfig";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      await account.deleteSession("current");
+    //   console.log("Previous session deleted");
+      const response = await account.createEmailPasswordSession(
+        email,
+        password
+      );
+      console.log(response); // You can handle the response as needed
+      window.location.href = '/loggedin'; // Redirect to logged-in page
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
+}
+
+export default Login;
